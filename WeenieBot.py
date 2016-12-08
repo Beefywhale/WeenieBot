3a2b1431d7c193ee4d96f2d056464e796a2a7274
Beefywhale <jonathan@integritypts.com>
2016-12-08 12:05:20 -0600

    Update WeenieBot.py

--- WeenieBot.py
+++ WeenieBot.py
@@ -10,8 +10,10 @@ import subprocess
 import sys
 import os
 import glob
-import modules.commands as commands
-import modules.botToken as botToken
+#import modules.commands as commands
+#import modules.botToken as botToken
+import botToken as botToken
+import commands as commands
 from google import search
 
 #changes! testing updates heehee
