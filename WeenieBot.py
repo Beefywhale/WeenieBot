02d99573d6720d166b93c938e8f5423babaea1fa
Beefywhale <jonathan@integritypts.com>
2016-12-07 20:51:33 -0600

    Update WeenieBot.py

--- WeenieBot.py
+++ WeenieBot.py
@@ -77,13 +77,13 @@ async def on_message(message):
     if message.content.lower().startswith('weeniebot'):
         await commands.cleverbot_logic(message, client)
 
-    if message.content == prefix + 'messages':
+    if message.content == pfix + 'messages':
         await commands.user_messages(message, client)
         
-    if message.content == prefix + 'purge':
+    if message.content == pfix + 'purge':
         await purge(message, client)
 
-    if message.content == prefix + 'update':
+    if message.content == pfix + 'update':
         g = git.cmd.Git()
         u = g.pull('-v')
         await client.send_message(message.channel, str(u))
@@ -92,7 +92,7 @@ async def on_message(message):
     if message.content.startswith(pfix + 'user'):
         await commands.user(message, client)
 
-    if message.content == prefix + 'admins':
+    if message.content == pfix + 'admins':
         await commands.admin_amount(message, client)
 
     if message.content.startswith(pfix + 'pokemon'):
@@ -101,32 +101,32 @@ async def on_message(message):
     if message.content.startswith(pfix + 'google'):
         await commands.google_search(message, client)
 
-    if message.content == prefix + 'good?':
+    if message.content == pfix + 'good?':
         await client.send_message(message.channel, 'I am as Fit as a Fiddle!')
     
     if message.content.startswith(pfix + 'sleep'):
         await commands.sleep(message, client)
 
-    if message.content == prefix + 'quotenumber':
+    if message.content == pfix + 'quotenumber':
         await commands.quote_amount(message, client)
 
-    if timer == 0 and message.content == prefix + 'turtles':
+    if timer == 0 and message.content == pfix + 'turtles':
         await client.send_message(message.channel, 'https://www.youtube.com/watch?v=o4PBYRN-ndI')
         timer = 1
-    elif  timer == 1 and message.content == prefix + 'turtles':
+    elif  timer == 1 and message.content == pfix + 'turtles':
         commands.cooldown(message, client)
 
     if message.content.lower() == 'hello weeniebot':
         await client.send_message(message.channel, message.author.mention + ' ' + 'Hello! I am WeenieBot, your robot friend, here to help you with your needs on this server! type !help to see what I can do for you!')
 
 
-    if message.content == prefix + 'quoteadd':
+    if message.content == pfix + 'quoteadd':
         await commands.quoteadd_logic(message, client)
 
 
-    if timer == 0 and message.content == prefix + 'quote':
+    if timer == 0 and message.content == pfix + 'quote':
         await commands.rand_quote(message, client)
-    elif  timer == 1 and message.content == prefix + 'quote':
+    elif  timer == 1 and message.content == pfix + 'quote':
         await commands.cooldown(message, client)
 
     if message.content.startswith(pfix + 'delquote'):
@@ -160,7 +160,7 @@ async def on_message(message):
             elif message.author.name not in admin:
                 await client.send_message(message.channel, 'ERROR You are not Admin')
 
-    if timer == 0 and message.content.split(' ')[0] == prefix + 'quote':
+    if timer == 0 and message.content.split(' ')[0] == pfix + 'quote':
         try:
             try:
                 open("quoteweenie.json","r")
@@ -174,7 +174,7 @@ async def on_message(message):
                 await client.send_message(message.channel, 'That quote doesn\'t exist!')
         except ValueError:
             pass
-    elif timer == 1 and message.content.split(' ')[0] == prefix + 'quote':
+    elif timer == 1 and message.content.split(' ')[0] == pfix + 'quote':
         try:
             try:
                 quote_number = int(message.content.strip(pfix + 'quote '))
@@ -187,7 +187,7 @@ async def on_message(message):
         except IndexError:
             await client.send_message(message.channel, 'That quote doesn\'t exist!')
 
-    if message.content == prefix + 'addadmin':
+    if message.content == pfix + 'addadmin':
         await commands.add_admin_logic(message, client)
 
     if message.content.startswith(pfix + 'deladmin'):
@@ -213,19 +213,19 @@ async def on_message(message):
         elif message.author.name not in admin:
             await client.send_message(message.channel, 'Not Admin!')
 
-    if message.content == prefix + 'help':
+    if message.content == pfix + 'help':
         r = lambda: random.randint(0,255)
         rr = ('0x%02X%02X%02X' % (r(),r(),r()))
         help_details = discord.Embed(title='Commands:', description='''
-prefix + quote --- picks a random quote to tell everyone.
-prefix + quote <number> --- picks a specific quote to tell everyone.
-prefix + quoteadd --- adds a new quote
-prefix + delquote <number> --- deletes specific quote
-prefix + sleep --- bot goes to sleep for 5 seconds.
-prefix + messages --- tells you how many messages there are in the channel you are in.
-prefix + admintest --- check if you are admin!
-prefix + deladmin --- deletes admin by user id
-prefix + addadmin <Persons Discord ID>--- adds admin by user id
+pfix + quote --- picks a random quote to tell everyone.
+pfix + quote <number> --- picks a specific quote to tell everyone.
+pfix + quoteadd --- adds a new quote
+pfix + delquote <number> --- deletes specific quote
+pfix + sleep --- bot goes to sleep for 5 seconds.
+pfix + messages --- tells you how many messages there are in the channel you are in.
+pfix + admintest --- check if you are admin!
+pfix + deladmin --- deletes admin by user id
+pfix + addadmin <Persons Discord ID>--- adds admin by user id
 hello weeniebot --- bot greets you.
 WeenieBot <question> --- asks weeniebot a question, that he will do his best to answer :)''', colour=int(rr, 16))
         help_details.set_author(name=message.server.me, icon_url=message.server.me.avatar_url)
@@ -234,3 +234,4 @@ WeenieBot <question> --- asks weeniebot a question, that he will do his best to 
 
 
 client.run(botToken.token)
+
