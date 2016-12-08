112e98f3c54e6f9fe63cdf48dd6fedb12b041352
Beefywhale <jonathan@integritypts.com>
2016-12-08 01:46:53 -0600

    Update WeenieBot.py

--- WeenieBot.py
+++ WeenieBot.py
@@ -237,9 +237,10 @@ async def on_message(message):
 
     if message.content.startswith(pfix + 'admintest'):
         open("adminweenie.json","r")
-        await client.send_message(message.channel, 'Hello Admin!')
-    elif message.author.name not in admin:
-        await client.send_message(message.channel, 'Not Admin!')
+        if message.author.name in admin:
+            await client.send_message(message.channel, 'Hello Admin!')
+        elif message.author.name not in admin:
+            await client.send_message(message.channel, 'Not Admin!')
 
     if message.content == pfix + 'help':
         r = lambda: random.randint(0,255)
