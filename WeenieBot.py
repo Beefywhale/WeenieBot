bcd4ccfe9b4859a2865afa8682374a60820a9594
Beefywhale <jonathan@integritypts.com>
2016-12-07 21:34:02 -0600

    Update WeenieBot.py

--- WeenieBot.py
+++ WeenieBot.py
@@ -14,8 +14,6 @@ import modules.botToken as botToken
 from google import search
 
 #changes! testing updates heehee
-with open("prefix.json", "r") as infile:
-    prefix = json.loads(infile.read())
 
 with open("quoteweenie.json","r") as infile:
     Quotes_All = json.loads(infile.read())
@@ -29,10 +27,10 @@ with open("quoteweenie.json", "w+") as outfile:
 with open("adminweenie.json", "w+") as outfile:
     outfile.write(json.dumps(admin))
 
+with open("prefix.json", "r") as infile:
+    prefix = json.loads(infile.read())
 with open("prefix.json", "w+") as outfile:
     outfile.write(json.dumps(prefix))
-
-    
 
 
 status = {
