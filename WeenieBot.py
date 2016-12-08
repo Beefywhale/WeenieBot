26e089698ccc74a0f2a774afd8e980815caba6d2
Beefywhale <jonathan@integritypts.com>
2016-12-08 15:03:08 -0600

    Update WeenieBot.py

--- WeenieBot.py
+++ WeenieBot.py
@@ -73,7 +73,8 @@ async def on_member_join(member):
 @client.event
 async def on_message(message):
     global timer
-    open("prefix.json", "r")
+    with open("prefix.json", "r") as infile:
+        prefix = json.loads(infile.read())
     pfix = commands.pfix
     await client.change_nickname(message.server.me, 'WeenieBot')
 
