9d9f1f4e09c9d66d97a816231bd0f7bbeb00323c
Beefywhale <jonathan@integritypts.com>
2016-12-09 13:54:55 -0600

    Update WeenieBot.py

--- WeenieBot.py
+++ WeenieBot.py
@@ -70,7 +70,6 @@ async def on_member_join(member):
 @client.event
 async def on_message(message):
     global timer
-    print(timer)
     with open("prefix.json", "r") as infile:
         prefix = json.loads(infile.read())
     pfix = commands.pfix
