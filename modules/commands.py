d3ca534fbdec4bbc28b74ae2d396dac9c52c4c29
Beefywhale <jonathan@integritypts.com>
2016-12-09 13:52:43 -0600

    Update commands.py

--- modules/commands.py
+++ modules/commands.py
@@ -41,7 +41,7 @@ status = {
 x33 = '%m-%d-%Y'
 counter1 = len(Quotes_All)
 counter2 = len(Quotes_All) - 1
-timer = client.timer
+timer = 0
 cb1 = cleverbot.Cleverbot()
 def bdel(s, r): return (s[len(r):] if s.startswith(r) else s)
 BASE_URL = 'http://pokeapi.co'
