97348b35d4514ffbcf195747181f8c395870efa6
Beefywhale <jonathan@integritypts.com>
2016-12-10 11:48:12 -0600

    Update WeenieBot.py

--- WeenieBot.py
+++ WeenieBot.py
@@ -42,7 +42,7 @@ class Weenie(discord.Client):
         elapsed = (time.time() - start)
         hours, rem = divmod(end-start, 3600)
         minutes, seconds = divmod(rem, 60)
-        self.uptime = "{:0>2}:{:0>2}:{:05.2f}".format(int(hours),int(minutes),seconds))
+        self.uptime = "{:0>2}:{:0>2}:{:05.2f}".format(int(hours),int(minutes),seconds)
         super().__init__(*args, **kwargs) 
 
 status = {
