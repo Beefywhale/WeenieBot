8303884668dba80f84521a0a6bdc5a91ea8069c5
Beefywhale <jonathan@integritypts.com>
2016-12-09 18:00:17 -0600

    Update WeenieBot.py

--- WeenieBot.py
+++ WeenieBot.py
@@ -163,6 +163,7 @@ async def on_message(message):
     if client.timer == 0 and message.content == pfix + 'quote':
         await commands.rand_quote(message, client)
     elif  client.timer == 1 and message.content == pfix + 'quote':
+        client.timer = 0
         await commands.cooldown(message, client)
 
     if message.content.startswith(pfix + 'delquote'):
