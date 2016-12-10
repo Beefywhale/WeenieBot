703a47fb7939576cd866df9d020dd2eb8babe9bc
Beefywhale <jonathan@integritypts.com>
2016-12-09 18:26:38 -0600

    Update WeenieBot.py

--- WeenieBot.py
+++ WeenieBot.py
@@ -161,6 +161,7 @@ async def on_message(message):
 
     if client.timer == 0 and message.content == pfix + 'quote':
         await commands.rand_quote(message, client)
+        client.timer = 1
     elif client.timer == 1 and message.content == pfix + 'quote':
         await commands.cooldown(message, client)
         
