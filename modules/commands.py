b856322a27ab0d902db6f35cac9f54cca3d81701
Beefywhale <jonathan@integritypts.com>
2016-12-11 00:05:49 -0600

    Update commands.py

--- modules/commands.py
+++ modules/commands.py
@@ -315,7 +315,7 @@ async def quoteadd_logic(message, client):
             await client.send_message(message.channel, 'Quote {} Added!'.format(counter1))
             counter1
             counter1 = len(Quotes_All)
-            Quotes_All.append(test.content)
+            Quotes_All.append(len(Quotes_All + 1) +': ' + test.content)
             with open("quoteweenie.json", "w+") as outfile:
                 outfile.write(json.dumps(Quotes_All))
     elif message.author.name not in admin:
