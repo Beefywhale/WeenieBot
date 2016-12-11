ae7992a6534b3df37a6e97951779f05cd32d9207
Beefywhale <jonathan@integritypts.com>
2016-12-11 12:52:45 -0600

    Update commands.py

--- modules/commands.py
+++ modules/commands.py
@@ -347,7 +347,7 @@ async def quote_amount(message, client):
     global counter2
     counter2 = len(Quotes_All)
     if message.server.id == '242887866730938378':
-        counter2 = len(Quotes_All - 1)
+        counter2 = str(len(Quotes_All) - 1)
         await client.send_message(message.channel, message.author.mention + ' ' + 'There Are {} Quotes!'.format(counter2))   
     else:
         await client.send_message(message.channel, message.author.mention + ' ' + 'There Are {} Quotes!'.format(counter2))
