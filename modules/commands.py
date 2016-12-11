552fbe1cd3a4112a9d2b21c9ce90e19f340fe678
Beefywhale <jonathan@integritypts.com>
2016-12-11 10:36:36 -0600

    Update commands.py

--- modules/commands.py
+++ modules/commands.py
@@ -144,7 +144,7 @@ async def quote_logic(message, client):
                 await client.send_message(message.channel, Quotes_All[quote_number])
             else:
                 await client.send_message(message.channel, Quotes_All[quote_number - 1])
-            client.timer = 1
+            #client.timer = 1
             #await asyncio.sleep(8)
             #client.timer = 0
         except IndexError:
