cd3f2107ef26deec0557ac91ecf0b45a563c519c
Beefywhale <jonathan@integritypts.com>
2016-12-11 18:32:50 -0600

    Update commands.py

--- modules/commands.py
+++ modules/commands.py
@@ -208,7 +208,7 @@ async def getPokemonData2(resource_url, message, client):
     async with aiohttp.ClientSession(loop=loop) as session:
         url = '{0}{1}'.format(BASE_URL, resource_url)
         html = await fetch(session, url)
-        if response.status == 200:
+        if html.status == 200:
             return json.loads(response.text)
             return None
         else:
