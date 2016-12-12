c46864665a828856be7805f0369ba04d88ae4229
Beefywhale <jonathan@integritypts.com>
2016-12-11 18:44:14 -0600

    Update commands.py

--- modules/commands.py
+++ modules/commands.py
@@ -208,8 +208,8 @@ async def getPokemonData2(resource_url, message, client):
     async with aiohttp.ClientSession(loop=loop) as session:
         url = '{0}{1}'.format(BASE_URL, resource_url)
         html = await fetch(session, url)
-        if url.status == 200:
-            return json.loads(session.text)
+        if response.status == 200:
+            return json.loads(response.text)
             return None
         else:
             await client.send_message(message.channel, 'PokeAPI has timed out! Maybe they are down :O')
