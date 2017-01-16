86c9f014804cc90601134d91685537d6276318c0
Beefywhale <jonathan@integritypts.com>
2017-01-16 15:52:22 -0600

    Changes to Minecraft api

--- modules/commands.py
+++ modules/commands.py
@@ -554,6 +554,7 @@ async def LoL_api(message, client):
             await client.send_message(message.channel, embed=summoner_details)
         else:
             await client.send_message(message.channel, 'Something went wrong with the API! :scream:')
+            
 async def minecraft(message, client):        
     r = lambda: random.randint(0,255)
     rr = ('0x%02X%02X%02X' % (r(),r(),r()))
@@ -566,7 +567,7 @@ async def minecraft(message, client):
             mc_details.add_field(name='Server Version: ', value=js['server']['name'])
             mc_details.add_field(name='Server Online:', value=js['online'])
             mc_details.add_field(name='Players:', value=str(js['players']['now']) + '/' + str(js['players']['max']))
-            mc_details.add_field(name='Description:', value=js['motd'])
+            mc_details.add_field(name='Description:', value=js['motd'].strip('§'))
             await client.send_message(message.channel, embed=mc_details)
         else:
             await client.send_message(message.channel, 'Something went wrong with the API! :scream:')
