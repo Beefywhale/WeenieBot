13d37071f8c25385fe140569c898ad20689ff532
Beefywhale <jonathan@integritypts.com>
2017-01-23 21:51:29 -0600

    Fixed repl(?)

--- modules/commands.py
+++ modules/commands.py
@@ -306,9 +306,9 @@ async def repl_logic(message, client):
                     client.repl = False
                 else:
                     if len(str(exec(evalt))) >= 1990:
-                        await client.send_message(message.channel, '```Python\n' + exec(evalt.content)[:1950] + '```' + '__Truncated!__')
+                        await client.send_message(message.channel, '```Python\n' + exec(str(evalt.content))[:1950] + '```' + '__Truncated!__')
                     else:
-                        await client.send_message(message.channel, '```Python\n' + exec(evalt.content) + '```')
+                        await client.send_message(message.channel, '```Python\n' + exec((evalt.content)) + '```')
             except Exception as x:
                 template = "An exception of type {0} occured. Arguments:\n{1!r}"
                 messagex = template.format(str(type(x).__name__), str(x))
