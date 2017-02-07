8f1728a850581f06cb21c7b2939820ea4a0f2dae
Beefywhale <jonathan@integritypts.com>
2017-02-07 10:50:29 -0600

    Update commands.py

--- modules/commands.py
+++ modules/commands.py
@@ -68,15 +68,15 @@ async def g_search_custom(message, client, search):
     loop = asyncio.get_event_loop()
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
     async with aiohttp.get('https://www.google.com/search?q={}&start=1&num=1'.format(search), headers=headers) as gr:
-    try: 
-        from BeautifulSoup import BeautifulSoup
-    except ImportError:
-        from bs4 import BeautifulSoup
-    html = await gr.text()
-    results = []
-    parsed_html = BeautifulSoup(html, "html.parser")
-    for item in parsed_html.find_all('h3', attrs={'class': 'r'}):
-        results.append(str(item.a['href']).replace('/url?q=', '').split('&sa=U&ved=')[0])
+        try: 
+            from BeautifulSoup import BeautifulSoup
+        except ImportError:
+            from bs4 import BeautifulSoup
+        html = await gr.text()
+        results = []
+        parsed_html = BeautifulSoup(html, "html.parser")
+        for item in parsed_html.find_all('h3', attrs={'class': 'r'}):
+            results.append(str(item.a['href']).replace('/url?q=', '').split('&sa=U&ved=')[0])
     await client.send_message(message.channel, 'Top result for `{}`: '.format(search) + ''.join(results[0]))
 
 async def ccipher(message, client):
