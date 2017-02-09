b1e56ea76614b70d8628dff65e593c8b12691f67
Beefywhale <jonathan@integritypts.com>
2017-02-08 20:38:47 -0600

    Small syntax error

--- WeenieBot.py
+++ WeenieBot.py
@@ -165,7 +165,7 @@ async def on_message(message):
     if message.content.startswith(client.pfix + 'join'):
         await client.voice_clients[message.server.id].join(message, client)
     if message.content.startswith(client.pfix + 'play'):
-        if message.server.id not in client.voiceQ:
+        if message.server.id not in client.voice_class_clients:
             client.voice_class_clients[message.server.id] = Voice = VoiceS(client)
         await client.voice_clients[message.server.id].play(message, client)
     if message.content.startswith(client.pfix + 'stop'):
