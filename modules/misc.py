bbc674bcb5d846cb6a67c166764d3f9a944ef422
Beefywhale <jonathan@integritypts.com>
2017-02-15 17:43:24 -0600

    Update misc.py

--- modules/misc.py
+++ modules/misc.py
@@ -24,7 +24,7 @@ commands.add_command(command_name='ping', command_function=ping_logic, alias='te
 
 '''Gets bots current uptime.'''
 async def uptime_logic(message, client):
-    await client.send_message(message.channel, "I have been awake for: " + time.process_time() - t)
+    await client.send_message(message.channel, "I have been awake for: " + str(time.process_time() - t))
 commands.add_command(command_name='uptime', command_function=uptime_logic)
 
 
