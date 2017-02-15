24cc570ccac08f1c0d3eb7d3b05b0a43fe9273a9
Beefywhale <jonathan@integritypts.com>
2017-02-15 11:23:11 -0600

    Attempt to fix cleverbot again(they changed api again >.<)

--- modules/custom_cleverbot/cleverbot.py
+++ modules/custom_cleverbot/cleverbot.py
@@ -28,7 +28,7 @@ class Cleverbot(object):
 
     HOST = "www.boibot.com"
     PROTOCOL = "http://"
-    RESOURCE = "/webservicemin?uc=777&botapi=see%20www.cleverbot.com%2Fapis&t=18205"
+    RESOURCE = "/webservicemin?uc=UseOfficialAPI&t=7295&re=yes&"
 
     def __init__(self, botapi, uc='3210'):
         """Cleverbot requests that bots identify themselves when
