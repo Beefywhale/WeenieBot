3fc726ff5c9b848113a4c77328df15fe153db4aa
Beefywhale <jonathan@integritypts.com>
2017-02-20 19:39:17 -0600

    Update misc.py

--- modules/misc.py
+++ modules/misc.py
@@ -21,7 +21,7 @@ def bdel(s, r): return (s[len(r):] if s.startswith(r) else s)
 '''A simple ping testing command.'''
 async def ping_logic(message, client):
     start2 = time.time()
-    await client.send_message(message.channel, 'Pong (Latency `{}`)'.format(time.time() - start2))
+    await client.send_message(message.channel, 'Pong')
 commands.add_command(command_name='ping', command_function=ping_logic, alias='test')
 
 '''displays bots current uptime.'''
