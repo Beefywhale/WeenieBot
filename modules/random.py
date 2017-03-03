2e00d35ce9adfa210ab52f13e1f5549459e0228b
Beefywhale <Beefywhale@users.noreply.github.com>
2017-03-02 20:01:13 -0600

    Update random.py

--- modules/random.py
+++ modules/random.py
@@ -54,7 +54,7 @@ async def wingdingcipher(message, client):
     for i in phrase:
         if i.lower() in alphabet:
             word = alphabet.index(i.lower())
-            x += '\' + wingdings[word]
+            x += '\\' + wingdings[word]
     await client.send_message(message.channel, x)
     print(x)
 commands.add_command(command_name='wingding', command_function=wingdingcipher, alias='wd')
